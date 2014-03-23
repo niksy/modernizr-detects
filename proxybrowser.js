4c700873e17f15d5f64a3a90bb463cbc67ff753a
Ivan Nikolić <niksy5@gmail.com>
2014-03-23 15:33:02 +0100

    Add "proxybrowser" test

---
+++ proxybrowser.js
@@ -0,0 +1,26 @@
+/*!
+{
+  "name": "Proxy browser",
+  "property": "proxybrowser",
+  "authors": ["Ivan Nikolić"],
+  "notes": [{
+    "name": "Opera Mini detection",
+    "href": "http://dev.opera.com/articles/view/opera-mini-and-javascript/#detectingmini"
+   },
+   {
+    "name": "Amazon Silk detection",
+    "href": "http://docs.aws.amazon.com/silk/latest/developerguide/user-agent.html#ua-detection"
+   }],
+  "tags": ["proxybrowser"]
+}
+!*/
+/* DOC
+
+Unfortunately, UA sniffing is only reliable solution for prxy browser detection.
+
+*/
+
+define(['Modernizr'], function( Modernizr ) {
+  rPlatform = /Opera Mini|Silk/i;
+  Modernizr.addTest('proxybrowser', rPlatform.test( navigator.userAgent ));
+});
