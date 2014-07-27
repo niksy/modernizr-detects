7242eba73309ca8511a020d6079caa3918e92f4d
Ivan Nikolić <niksy5@gmail.com>
2014-07-27 15:41:58 +0200

    Add test

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,15 +29,42 @@ module.exports = function ( grunt ) {
 					'feature-detects/**/*.js'
 				]
 			}
+		},
+
+		modernizr: {
+			dist: {
+				uglify: false,
+				'dest': 'test/modernizr.js',
+				'options': [
+					'setClasses',
+					'addTest',
+					'html5printshiv',
+					'testProp'
+				],
+				'excludeTests': [
+					'contains',
+					'flash'
+				],
+				'customTests': [
+					'feature-detects/adblock.js',
+					'feature-detects/appearance.js',
+					'feature-detects/ie.js',
+					'feature-detects/print.js',
+					'feature-detects/proxybrowser.js',
+					'feature-detects/xflash.js'
+				]
+			}
 		}
 
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-bump');
+	grunt.loadNpmTasks('grunt-modernizr');
 
 	grunt.registerTask('stylecheck', ['jshint:main']);
 	grunt.registerTask('default', ['stylecheck']);
+	grunt.registerTask('test', ['modernizr']);
 	grunt.registerTask('releasePatch', ['bump-only:patch', 'default', 'bump-commit']);
 	grunt.registerTask('releaseMinor', ['bump-only:minor', 'default', 'bump-commit']);
 	grunt.registerTask('releaseMajor', ['bump-only:major', 'default', 'bump-commit']);
