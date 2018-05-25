f091157d9aeaf8a11e7f0ba1095ead5de7105a47
Ivan Nikolić <niksy5@gmail.com>
2018-05-25 15:40:01 +0200

    Improve ad blocker detection
    Ref. https://github.com/nsi88/adblockdetect
    Ref. https://github.com/wmcmurray/just-detect-adblock

--- feature-detects/adblock.js
+++ feature-detects/adblock.js
@@ -9,41 +9,100 @@
 !*/
 /* jshint expr: true */
 
-define(['Modernizr', 'createElement', 'docElement', 'addTest'], function ( Modernizr, createElement, docElement, addTest ) {
+define(['Modernizr', 'createElement', 'addTest'], function ( Modernizr, createElement, addTest ) {
 
 	Modernizr.addAsyncTest(function () {
 
-		var adblockActive = false;
-		var el = createElement('img');
-		el.src = '//ads.modernizr';
-		el.style.width = '1px';
-		el.style.height = '1px';
-		el.style.position = 'absolute';
+		var adBlockActive = false;
+		var detectionTimerCount = 0;
+		var numberOfChecks = 5;
+		var container = document.body;
+		var el, detectionTimer, bodyReadyTimer;
 
-		// IE8 complains if we try to append element to HTML element
-		// Since it certainly doesn’t have ad blocker, we automatically return false
-		if ( document.all && !document.addEventListener ) {
+		function isBodyReady () {
+			return document.body !== null;
+		}
 
-			addTest('adblock', false);
+		function detectAdBlocker () {
 
-		} else {
+			var computedStyle;
 
-			docElement.appendChild(el);
+			if ( typeof window.getComputedStyle !== 'undefined' ) {
+				computedStyle = window.getComputedStyle(el, null);
+			}
 
-			setTimeout(function () {
-
-				adblockActive = !docElement.contains(el) || el.style.display === 'none';
-
-				addTest('adblock', adblockActive);
-
-				if ( adblockActive ) {
-					docElement.removeChild(el);
-				}
-				el = null;
-
-			}, 200);
+			try {
+				adBlockActive = (
+					container.getAttribute('abp') !== null ||
+					(computedStyle ? computedStyle.getPropertyValue('display') : el.style.display) === 'none' ||
+					(computedStyle ? computedStyle.getPropertyValue('visibility') : el.style.visibility) === 'hidden' ||
+					el.offsetParent === null ||
+					el.offsetHeight === 0 ||
+					el.offsetLeft === 0 ||
+					el.offsetTop === 0 ||
+					el.offsetWidth === 0 ||
+					el.clientHeight === 0 ||
+					el.clientWidth === 0
+				);
+			} catch ( err ) {
+				adBlockActive = false;
+			}
 
 		}
+
+		function insertDummyElement () {
+			container.appendChild(el);
+		}
+
+		function removeDummyElement () {
+			container.removeChild(el);
+			el = null;
+		}
+
+		function bodyReadyActions () {
+
+			insertDummyElement();
+
+			detectAdBlocker();
+
+			if ( adBlockActive ) {
+				addTest('adblock', adBlockActive);
+				removeDummyElement();
+				return;
+			}
+
+			detectionTimer = setInterval(function () {
+				if (
+					detectionTimerCount >= numberOfChecks ||
+					adBlockActive
+				) {
+					clearInterval(detectionTimer);
+					addTest('adblock', adBlockActive);
+					removeDummyElement();
+					return;
+				}
+				detectionTimerCount = detectionTimerCount + 1;
+				detectAdBlocker();
+			}, 100);
+
+		}
+
+		el = createElement('div');
+		el.className = 'pub_300x250 pub_300x250m pub_728x90 text-ad textAd text_ad text_ads text-ads';
+		el.setAttribute('style', 'position: absolute; top: -10px; left: -10px; width: 1px; height: 1px;');
+
+		if ( isBodyReady() ) {
+			bodyReadyActions();
+			return;
+		}
+
+		bodyReadyTimer = setInterval(function () {
+			if ( isBodyReady() ) {
+				clearInterval(bodyReadyTimer);
+				container = document.body;
+				bodyReadyActions();
+			}
+		}, 16);
 
 	});
 
